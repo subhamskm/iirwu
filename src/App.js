7d5dd71d01d8c4e63d9b81da815e0cca1ba82834
Subham Kumar <shubham.kumar.ggps@gmail.com>
2020-02-16 02:04:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Heading from './components/heading/heading';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Heading />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/heading/heading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './heading.css';
+
+class Heading extends Component {
+    render() {
+      return (
+        <h1>IIRWU</h1>
+      );
+    }
+  }
+  
+  export default Heading;
